cbbe5e08ebe0cf3def612830ca056a77a5e7e408
FRANCISCO JAVIER FERNÁNDEZ COLLADOS <3605143@alu365.murciaeduca.es>
2024-10-16 20:46:09 +0200

    FrmPacientes casi terminado

---
+++ WinForm/Frm/Pacientes/FrmPacientesRead.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WinForm.Frm.Pacientes
+{
+    partial class FrmPacientesRead
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPacientesRead));
+            toolStripFrmPacientes = new ToolStrip();
+            tsbFrmPacientesCrear = new ToolStripButton();
+            tsbFrmPacientesEditar = new ToolStripButton();
+            tsbFrmPacientesEliminar = new ToolStripButton();
+            dataGridView1 = new DataGridView();
+            toolStripFrmPacientes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // toolStripFrmPacientes
+            // 
+            toolStripFrmPacientes.Items.AddRange(new ToolStripItem[] { tsbFrmPacientesCrear, tsbFrmPacientesEditar, tsbFrmPacientesEliminar });
+            toolStripFrmPacientes.Location = new Point(0, 0);
+            toolStripFrmPacientes.Name = "toolStripFrmPacientes";
+            toolStripFrmPacientes.Size = new Size(800, 25);
+            toolStripFrmPacientes.TabIndex = 0;
+            toolStripFrmPacientes.Text = "Menú Pacientes";
+            // 
+            // tsbFrmPacientesCrear
+            // 
+            tsbFrmPacientesCrear.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            tsbFrmPacientesCrear.Image = (Image)resources.GetObject("tsbFrmPacientesCrear.Image");
+            tsbFrmPacientesCrear.ImageTransparentColor = Color.Magenta;
+            tsbFrmPacientesCrear.Name = "tsbFrmPacientesCrear";
+            tsbFrmPacientesCrear.Size = new Size(23, 22);
+            tsbFrmPacientesCrear.Text = "Añadir";
+            // 
+            // tsbFrmPacientesEditar
+            // 
+            tsbFrmPacientesEditar.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            tsbFrmPacientesEditar.Image = (Image)resources.GetObject("tsbFrmPacientesEditar.Image");
+            tsbFrmPacientesEditar.ImageTransparentColor = Color.Magenta;
+            tsbFrmPacientesEditar.Name = "tsbFrmPacientesEditar";
+            tsbFrmPacientesEditar.Size = new Size(23, 22);
+            tsbFrmPacientesEditar.Text = "Editar";
+            // 
+            // tsbFrmPacientesEliminar
+            // 
+            tsbFrmPacientesEliminar.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            tsbFrmPacientesEliminar.Image = (Image)resources.GetObject("tsbFrmPacientesEliminar.Image");
+            tsbFrmPacientesEliminar.ImageTransparentColor = Color.Magenta;
+            tsbFrmPacientesEliminar.Name = "tsbFrmPacientesEliminar";
+            tsbFrmPacientesEliminar.Size = new Size(23, 22);
+            tsbFrmPacientesEliminar.Text = "Eliminar";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(0, 25);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(800, 425);
+            dataGridView1.TabIndex = 1;
+            // 
+            // FrmPacientesRead
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(toolStripFrmPacientes);
+            Name = "FrmPacientesRead";
+            Text = "FrmPacientesRead";
+            toolStripFrmPacientes.ResumeLayout(false);
+            toolStripFrmPacientes.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ToolStrip toolStripFrmPacientes;
+        private ToolStripButton tsbFrmPacientesCrear;
+        private ToolStripButton tsbFrmPacientesEditar;
+        private ToolStripButton tsbFrmPacientesEliminar;
+        private DataGridView dataGridView1;
+    }
+}
