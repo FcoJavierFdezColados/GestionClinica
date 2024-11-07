54ed63f7473d2e34c946b86f5b594a8a9e71ad4a
FRANCISCO JAVIER FERNÁNDEZ COLLADOS <3605143@alu365.murciaeduca.es>
2024-11-07 20:46:39 +0100

    Doctores update casi hecho

--- WinForm/Frm/Doctores/FrmDoctoresUpdate.Designer.cs
+++ WinForm/Frm/Doctores/FrmDoctoresUpdate.Designer.cs
@@ -30,6 +30,12 @@
         {
             btnDoctoresUpdateAceptar = new Button();
             btnDoctoresUpdateCancel = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
             SuspendLayout();
             // 
             // btnDoctoresUpdateAceptar
@@ -54,6 +60,54 @@
             btnDoctoresUpdateCancel.Text = "Cancelar";
             btnDoctoresUpdateCancel.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(28, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(51, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 94);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Apellidos";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 168);
+            label3.Name = "label3";
+            label3.Size = new Size(124, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Número de Colegiado";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(142, 165);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(646, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(177, 86);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(141, 39);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 7;
+            // 
             // FrmDoctoresUpdate
             // 
             AcceptButton = btnDoctoresUpdateAceptar;
@@ -61,16 +115,29 @@
             AutoScaleMode = AutoScaleMode.Font;
             CancelButton = btnDoctoresUpdateCancel;
             ClientSize = new Size(800, 450);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(btnDoctoresUpdateCancel);
             Controls.Add(btnDoctoresUpdateAceptar);
             Name = "FrmDoctoresUpdate";
             Text = "FrmDoctoresUpdate";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button btnDoctoresUpdateAceptar;
         private Button btnDoctoresUpdateCancel;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
     }
 }
